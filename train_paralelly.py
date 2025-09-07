55ef289263d8fff408690aa038e52d6ff65a83a9
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-09-07 10:19:38 +0530

    debug train

--- train_paralelly.py
+++ train_paralelly.py
@@ -22,34 +22,34 @@ p = subprocess.Popen(trl_cmd, stdout=trl_log, stderr=subprocess.STDOUT)
 processes.append(p)
 
 
- 
-for coin in COINS:
-    # tst_train
-    tst_cmd = [
-        "python", os.path.join(full_path, "tst_train.py"),
-        "--coin", coin,
-        "--epochs", "10",
-        "--batch_size", "200",
-        "--seq_len", "30",
-        "--max_time", str(max_time) 
-    ]
-    tst_log = open(os.path.join(logs_path, f"{coin}_tst.log"), "w")
-    p = subprocess.Popen(tst_cmd, stdout=tst_log, stderr=subprocess.STDOUT)
-    processes.append(p)
+#  python --epochs 10 --batch_size 12 --max_time 3000
+# for coin in COINS:
+#     # tst_train
+#     tst_cmd = [
+#         "python", os.path.join(full_path, "tst_train.py"),
+#         "--coin", coin,
+#         "--epochs", "10",
+#         "--batch_size", "200",
+#         "--seq_len", "30",
+#         "--max_time", str(max_time) 
+#     ]
+#     tst_log = open(os.path.join(logs_path, f"{coin}_tst.log"), "w")
+#     p = subprocess.Popen(tst_cmd, stdout=tst_log, stderr=subprocess.STDOUT)
+#     processes.append(p)
 
-    # lgb_train
-    lgb_cmd = [
-        "python", os.path.join(full_path, "lgb_train.py"),
-        "--coin", coin,
-        "--epochs", "200"
-    ]
-    lgb_log = open(os.path.join(logs_path, f"{coin}_lgbm.log"), "w")
-    p = subprocess.Popen(lgb_cmd, stdout=lgb_log, stderr=subprocess.STDOUT)
-    processes.append(p)
+#     # lgb_train
+#     lgb_cmd = [
+#         "python", os.path.join(full_path, "lgb_train.py"),
+#         "--coin", coin,
+#         "--epochs", "200"
+#     ]
+#     lgb_log = open(os.path.join(logs_path, f"{coin}_lgbm.log"), "w")
+#     p = subprocess.Popen(lgb_cmd, stdout=lgb_log, stderr=subprocess.STDOUT)
+#     processes.append(p)
 
 
-# Optional: wait for all to finish (or just leave them running in parallel)
-for p in processes:
-    p.wait()
+# # Optional: wait for all to finish (or just leave them running in parallel)
+# for p in processes:
+#     p.wait()
 
 print("All training scripts started in parallel.")
