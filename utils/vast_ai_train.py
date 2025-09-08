e6d91fd0a208cd4e181a75d3877b4fb88b432fd0
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-09-08 10:58:26 +0530

    debug train

--- utils/vast_ai_train.py
+++ utils/vast_ai_train.py
@@ -126,7 +126,7 @@ onstart_cmd = (
     f"{export_string} && cd /app && "
     f"git clone https://github.com/FrozenWolf-Cyber/crypto-mlops && pip install wandb && wandb login 283c41dda88b658ba85c2d8ee7d37230f3341d8c &&"
     f"cd crypto-mlops &&"
-    f"python trainer.train_paralelly.py"
+    f"python -m trainer.train_paralelly"
 )
 
 print(onstart_cmd)
