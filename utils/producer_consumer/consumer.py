7391e404df3d19bc5ff30ed5664dc90363b14212
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-09-23 00:16:19 -0700

    airflow fix vast ai debug

--- utils/producer_consumer/consumer.py
+++ utils/producer_consumer/consumer.py
@@ -35,8 +35,9 @@ def get_predictions(inp, crypto, model, version):
         
     if len(inp) != 0:
         params = {"model_name": f"{crypto.lower()}_{model.lower()}", "version": int(version[1:])-1}
-        print(inp)
+        print(inp[:10])
         print(params)
+        inp = inp.tolist()
         pred = requests.post(url, params=params, json=inp)
         pred = pred.json()['predictions']
         return pred
