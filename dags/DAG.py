1759eea2ada3069d66523a7e6c2bf923ad2fd2a9
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-09-24 10:06:57 -0700

    airflow trl scrapper increase limit;

--- dags/DAG.py
+++ dags/DAG.py
@@ -219,8 +219,8 @@ def create_dag1():
         start_date=start_date,
         catchup=False,
         max_active_runs=1,
-        dagrun_timeout=timedelta(hours=3),
-        on_failure_callback=cleanup_on_timeout
+        # dagrun_timeout=timedelta(hours=3),
+        # on_failure_callback=cleanup_on_timeout
     ) as dag:
 
         start_pretrain = BashOperator(
@@ -341,16 +341,16 @@ def create_dag1():
         for model in models:
             monitor_tasks[model] >> [post_tasks[model], skip_task]
         
-        final_kill = BashOperator(
-            task_id="final_kill_kill_vast_ai_instances",
-            bash_command="PYTHONPATH=..:$PYTHONPATH python -m utils.utils.kill_vast_ai_instances",
-            trigger_rule=TriggerRule.ALL_DONE,
-                on_execute_callback=log_start,
-                on_success_callback=log_success,
-                on_failure_callback=log_failure,
-        )
-        post_tasks_list = list(post_tasks.values())
-        [post_tasks_list, skip_task] >> final_kill
+        # final_kill = BashOperator(
+        #     task_id="final_kill_kill_vast_ai_instances",
+        #     bash_command="PYTHONPATH=..:$PYTHONPATH python -m utils.utils.kill_vast_ai_instances",
+        #     trigger_rule=TriggerRule.ALL_DONE,
+        #         on_execute_callback=log_start,
+        #         on_success_callback=log_success,
+        #         on_failure_callback=log_failure,
+        # )
+        # post_tasks_list = list(post_tasks.values())
+        # [post_tasks_list, skip_task] >> final_kill
 
     return dag
 
