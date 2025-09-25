7b57c7621fea9e183b0ba611dde9949b17290d8e
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-09-24 20:53:59 -0700

    airflow dag revert logging

--- utils/producer_consumer/consumer_start.py
+++ utils/producer_consumer/consumer_start.py
@@ -4,10 +4,14 @@ from ..artifact_control.s3_manager import S3Manager
 from ..trainer.train_utils import download_s3_dataset
 from .consumer_utils import state_write, state_checker, STATE_DIR, delete_all_states
 import time
+import logging
+log = logging.getLogger(__name__)
+
 manager = S3Manager()
-print("Cleaning up old state files...")
+log.info("Cleaning up old state files...")
+log.info("Cleaning up old state files...")
 delete_all_states()
-print("Downloading initial datasets...")
+log.info("Downloading initial datasets...")
 download_s3_dataset("BTCUSDT", trl_model=True)
 
 def create_dir(path):
@@ -21,17 +25,17 @@ versions = ["v1", "v2", "v3"]
 
 def create_consumer(crypto: str, model: str, version: str):
     if os.path.exists(os.path.join(STATE_DIR, f"{crypto}_{model}_{version}.json")):
-        print(f"[WARNING] State file for {crypto} {model} {version} already exists, removing it first.")
+        log.info(f"[WARNING] State file for {crypto} {model} {version} already exists, removing it first.")
         os.remove(os.path.join(STATE_DIR, f"{crypto}_{model}_{version}.json"))
     """Download datasets and launch consumer."""
-    print(f"[CREATE] Preparing consumer for {crypto} {model} {version}")
+    log.info(f"[CREATE] Preparing consumer for {crypto} {model} {version}")
 
     cmd = [
     "bash", "-c",
     f"PYTHONPATH=..:$PYTHONPATH python -m utils.producer_consumer.consumer.py"
     f"--crypto {crypto} --model {model} --version {version}"
     ]
-    print("[CREATE] Launching:", " ".join(cmd))
+    log.info("[CREATE] Launching:", " ".join(cmd))
     subprocess.Popen(
     cmd,
     stdout=subprocess.DEVNULL,
@@ -45,14 +49,14 @@ def create_consumer(crypto: str, model: str, version: str):
 procs = []
 for crypto in cryptos:
     for model in models:
-        print(f"[INFO] Downloading available predictions for {crypto} {model}...")
+        log.info(f"[INFO] Downloading available predictions for {crypto} {model}...")
         manager.download_available_predictions(crypto, model)
         versions_ = manager.get_existing_versions(crypto, model)
-        print(f"[INFO] Existing versions for {crypto} {model}: {versions_}")
+        log.info(f"[INFO] Existing versions for {crypto} {model}: {versions_}")
         for version in versions[:len(versions_)]:
-            print(f"[INFO] Creating consumer for {crypto} {model} {version}...")
+            log.info(f"[INFO] Creating consumer for {crypto} {model} {version}...")
             create_consumer(crypto, model, version)
 
             while state_checker(crypto, model, version) == "unknown":
                 time.sleep(0.5)
-            print(f"State file for {crypto} {model} {version} exists, consumer launched.")
+            log.info(f"State file for {crypto} {model} {version} exists, consumer launched.")
