a1c8218102621aeb301c681eb7a7ea0ac607d8db
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-09-26 02:04:40 -0700

    consumer missing pred fix

--- utils/producer_consumer/consumer.py
+++ utils/producer_consumer/consumer.py
@@ -140,8 +140,8 @@ def build_pipeline(app, crypto, model, version):
             # print(missing_pred_dates_db.values[0] in missing_pred_dates_db_dates)
             # print(missing_pred_dates.values[0] in missing_pred_dates_db_dates)
             # print(missing_pred_dates_db_dates[:5])
-            print(missing_pred_dates_db_dates[0]==missing_pred_dates.values[0])
-            print(missing_pred_dates_db_dates[0], missing_pred_dates.values[0])
+            # print(missing_pred_dates_db_dates[0]==missing_pred_dates.values[0])
+            # print(missing_pred_dates_db_dates[0], missing_pred_dates.values[0])
             for d in tqdm(missing_pred_dates.values):
 
                 if d not in pos_map:
