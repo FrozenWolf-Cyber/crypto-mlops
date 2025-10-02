2f54bb15e9d93df1892868f3a23e8c4c558ef58a
Gokul Adethya <frozenwolf2003.com@gmail.com>
2025-10-01 18:07:14 -0700

    fix datetime

--- utils/producer_consumer/producer.py
+++ utils/producer_consumer/producer.py
@@ -199,7 +199,7 @@ def main():
                 send_df_to_quix(symbol, records, producer)
                 
                 records["open_time"] = pd.to_datetime(records["open_time"], format='%Y-%m-%d %H:%M:%S')
-                
+                records["open_time"] = pd.to_datetime(records["open_time"], utc=True)
                 print(f"Updated last time for {symbol} to {last_times[symbol]}")
                 
                 #### insert into csv
